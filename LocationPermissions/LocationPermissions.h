33148748f4f2ed1f8ee4fe6282bb0fe8289baed4
Sergey Kostyan <>
2018-04-27 20:33:54 +0300

    move to open

---
+++ LocationPermissions/LocationPermissions.h
@@ -0,0 +1,19 @@
+//
+//  LocationPermissions.h
+//  LocationPermissions
+//
+//  Created by Sergey Kostyan on 24.04.2018.
+//  Copyright © 2018 Sergey Kostyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationPermissions.
+FOUNDATION_EXPORT double LocationPermissionsVersionNumber;
+
+//! Project version string for LocationPermissions.
+FOUNDATION_EXPORT const unsigned char LocationPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationPermissions/PublicHeader.h>
+
+
---
+++ LocationPermissions/Sources/LocationPermissions.swift
@@ -0,0 +1,62 @@
+//
+//  LocationServicePermissions.swift
+//  SKUtilsSwift
+//
+//  Created by Sergey Kostyan on 25.09.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import SKServicePermissions
+
+public enum LocationAuthorizationType: String {
+    case requestAlwaysAuthorization
+    case requestWhenInUseAuthorization
+}
+
+open class LocationPermissions: NSObject {
+
+    private var authType: LocationAuthorizationType
+    private var locationManager: CLLocationManager
+    private var requestPermissionsHandler: ((PermissionsState) -> Void)?
+
+    public init(authType: LocationAuthorizationType = .requestAlwaysAuthorization) {
+        self.authType = authType
+        locationManager = CLLocationManager()
+    }
+    
+}
+
+// MARK: - ServicePermissions -
+
+extension LocationPermissions: ServicePermissions {
+    
+    public typealias PermissionsState = CLAuthorizationStatus
+    
+    public func requestPermissions(handler: @escaping (PermissionsState) -> Void) {
+        requestPermissionsHandler = handler
+        locationManager.delegate = self
+        switch authType {
+        case .requestAlwaysAuthorization:
+            locationManager.requestAlwaysAuthorization()
+        case .requestWhenInUseAuthorization:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    public func permissionsState() -> PermissionsState {
+        return CLLocationManager.authorizationStatus()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate -
+
+extension LocationPermissions: CLLocationManagerDelegate {
+
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // WARNING: - delegate method called for notDetermined state too -
+        requestPermissionsHandler?(permissionsState())
+    }
+
+}
